357a85585fded031dc43adb2b7739a4f7a10d6dd
Glitch (0xerr) <none>
2018-03-09 17:05:18 +0000

    Nodes as black/blue by default. Lets think about custom colors at another time.

--- public/0xerr/engine/factory/node.js
+++ public/0xerr/engine/factory/node.js
@@ -1,11 +1,10 @@
 import {ActiveComponent} from '../components/active.js';
-import {Color} from '../../renderer/graphics/color.js';
 import {EntityManager} from '../entity/manager.js';
 import {NodeComponent, NodeType} from '../components/node.js';
+import {BLACK, BLUE_BRIGHT} from '../common/palette.js';
 import {SpatialComponent} from '../components/spatial.js';
 import {StyleComponent} from '../components/style.js';
 import {ij} from '../../injection/api.js';
-import {randomInt} from '../../stdlib/random.js';
 
 class NodeFactory {
   constructor(manager = ij(EntityManager)) {
@@ -17,9 +16,7 @@ class NodeFactory {
     this.manager.add(id, new ActiveComponent(true));
     this.manager.add(id, new NodeComponent(NodeType.HVAC));
     this.manager.add(id, new SpatialComponent(10, 10, 40, 20));
-    this.manager.add(id, new StyleComponent(
-        new Color().setHSV(0, 0, 1).lock(),
-        new Color().setHSV(randomInt(360), 1, .5).lock()));
+    this.manager.add(id, new StyleComponent(BLUE_BRIGHT, BLACK));
   }
 }
 
