0df1b243ca544fa712317807b087765f40a32696
Glitch (0xerr) <none>
2018-03-21 21:59:39 +0000

    Implement a 32bit xorwow-based RNG that provides multiple RNG channels based on a single seed.

--- public/0xerr/stdlib/random.js
+++ public/0xerr/stdlib/random.js
@@ -1,22 +1,66 @@
-function randomDev(center, dev) {
-  return randomRange(center - dev, center + dev);
+const UINT32_MASK = 0xffffffff;
+
+const DEFAULT_SEED = Uint32Array.from([
+    0x12345678,
+    0x56789abc,
+    0x9abcdef0,
+    0xdef01234,
+    0x12345678,
+]);
+
+class RandomXorWow {
+  constructor(seed = DEFAULT_SEED) {
+    this.state = Uint32Array.from(seed);
+  }
+  
+  next() {
+    let s;
+    let t = this.state[0] & UINT32_MASK;
+    t ^= (t >> 2) & UINT32_MASK;
+    t ^= (t << 1) & UINT32_MASK;
+    this.state[3] = this.state[2] & UINT32_MASK;
+    this.state[2] = this.state[1] & UINT32_MASK;
+    this.state[1] = s = this.state[0] & UINT32_MASK;
+    t ^= (s) & UINT32_MASK;
+    t ^= (s << 4) & UINT32_MASK;
+    this.state[0] = t & UINT32_MASK;
+    this.state[4] = (this.state[4] + 0x587c5) & UINT32_MASK;
+    return (t + this.state[4]) & UINT32_MASK;
+  }
+  
+  random() {
+    return this.next() / UINT32_MASK;
+  }
+  
+  randomRange(min, max) {
+    const delta = max - min;
+    return Math.round(min + (this.random() * delta));
+  }
+  
+  randomBoolean() {
+    return this.random() > 0.5;
+  }
 }
 
-function randomInt(max) {
-  return randomRange(0, Math.ceil(max));
+
+class Random {
+  constructor(seed) {
+    this.generator = new RandomXorWow(seed);
+    this.channels = new Map();
+  }
+  
+  get(channel) {
+    if (!this.channels.has(channel)) {
+      this.channels.set(channel,
+          new RandomXorWow([
+              this.generator.next(),
+              this.generator.next(),
+              this.generator.next(),
+              this.generator.next(),
+              this.generator.next()]));
+    }
+    return this.channels.get(channel);
+  }
 }
 
-function randomRange(min, max) {
-  return min + Math.round(Math.random() * (max - min));
-}
-
-function randomRangeDouble(min, max) {
-  return min + (Math.random() * (max - min));
-}
-
-export {
-  randomDev,
-  randomInt,
-  randomRange,
-  randomRangeDouble,
-};
+export {Random};
