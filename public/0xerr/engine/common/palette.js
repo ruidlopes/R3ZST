9e1f8a0324e7e2d3083b50a67d082ebf24cf9891
Glitch (0xerr) <none>
2018-04-18 17:12:14 +0000

    Another round of color tweaks

--- public/0xerr/engine/common/palette.js
+++ public/0xerr/engine/common/palette.js
@@ -7,12 +7,12 @@ const BLUE_FADED  = new Color().setHSV(195, 1, 0.75).lock();
 const BLUE_FADED2 = new Color().setHSV(195, 1, 0.5).lock();
 const BLUE_FADED3 = new Color().setHSV(195, 1, 0.25).lock();
 
-const HIGHLIGHT_BRIGHT = new Color().setHSV(145, 1, 1).lock();
-const HIGHLIGHT_FADED = new Color().setHSV(145, 1, 0.75).lock();
+const HIGHLIGHT_BRIGHT = new Color().setHSV(145, 1, 0.85).lock();
+const HIGHLIGHT_FADED = new Color().setHSV(145, 1, 0.65).lock();
 
 const RED_ORANGE_BRIGHT = new Color().setHSV(25, 1, 1).lock();
 const RED_ORANGE_FADED = new Color().setHSV(25, 1, 0.75).lock();
-const RED_MAGENTA_BRIGHT = new Color().setHSV(330, 1, 1).lock();
+const RED_MAGENTA_BRIGHT = new Color().setHSV(330, 1, 0.9).lock();
 
 const WHITE = new Color().setHSV(45, 0.25, 0.9).lock();
 
